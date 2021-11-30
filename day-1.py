0ca67e841064244e67b69eec6f3551165fbfac89
Cameron <cameron.wragg@glencore.co.uk>
2021-11-30 17:22:36 +0000

    Renaming package and updating test script

--- day-1.py
+++ day-1.py
@@ -1,7 +1,5 @@
-import pandas as pd
-
-# import numpy as np
+from packages.aoc_helper import Input
 
 if __name__ == "__main__":
-    df = pd.read_csv("data/test.csv")
-    print(df.columns.tolist()[0])
+    test_string = Input.readto_string("./data/test.txt")
+    print(test_string)
