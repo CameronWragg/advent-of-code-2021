d04f33346e9f7bf282d0a0d3ce3794bdf6ffdf98
Cameron <cameron.wragg@glencore.co.uk>
2021-11-30 17:09:50 +0000

    Adding Input class to helper package

---
+++ packages/aoc-helper.py
@@ -0,0 +1,30 @@
+from typing import List, Union
+from numpy import array_split, ndarray
+import re
+
+
+class Input:
+    @staticmethod
+    def readto_string(file: str) -> str:
+        return open(file).read().rstrip("\n")
+
+    @staticmethod
+    def readto_list(file: str, to_int: bool = False) -> Union[List[str], List[int]]:
+        _list = Input.readto_string(file).split("\n")
+        if not to_int:
+            return _list
+        else:
+            return list(map(lambda l: int(l.rstrip("\n")), _list))
+
+    @staticmethod
+    def readto_matrix(file: str, cols: int) -> List[ndarray]:
+        _list = Input.readto_list(file, to_int=True)
+        return array_split(_list, cols)
+
+    @staticmethod
+    def readto_list_re(file: str, re_pattern: str, max_split: int = None) -> List[str]:
+        _string = Input.readto_string(file)
+        if not max_split:
+            return re.split(re_pattern, _string)
+        else:
+            return re.split(re_pattern, _string, max_split)
