8a71726deb3c4c7c114efc47b1eece30b5e844b1
Cameron <cameron.wragg@glencore.co.uk>
2021-12-01 14:08:41 +0000

    Further optimisations on day-1

--- day-1.py
+++ day-1.py
@@ -4,23 +4,15 @@ from functools import reduce
 from operator import add
 
 
-def _is_higher(input: Tuple[int]) -> Literal[1, 0]:
-    return input[1] > input[0]
-
-
-def _sum_three(input: Tuple[int]) -> int:
-    return sum(input)
-
-
 def part1(file: str, input_list: list = None) -> int:
     _input = Input.readto_list(file, to_int=True) if not input_list else input_list
-    _answer = reduce(add, map(_is_higher, zip(_input, _input[1:])))
+    _answer = reduce(add, map(lambda i: i[1] > i[0], zip(_input, _input[1:])))
     return _answer
 
 
 def part2(file: str) -> int:
     _input = Input.readto_list(file, to_int=True)
-    _answer = part1(None, list(map(_sum_three, zip(_input, _input[1:], _input[2:]))))
+    _answer = part1(None, list(map(sum, zip(_input, _input[1:], _input[2:]))))
     return _answer
 
 
