2f040bd41153705a965f66dcc6d0bea082326a9c
Cameron <cameron.wragg@glencore.co.uk>
2021-12-01 13:30:06 +0000

    Optimizing day 1 solution

--- day-1.py
+++ day-1.py
@@ -1,33 +1,27 @@
-from typing import Literal
+from typing import Literal, Tuple
 from packages.aoc_helper import Input
-from collections import deque
 from functools import reduce
 from operator import add
 
 
-def _is_higher(input: int) -> Literal[1, 0]:
+def _is_higher(input: Tuple[int]) -> Literal[1, 0]:
     return input[1] > input[0]
 
 
-def part1(file: str) -> int:
-    _input = Input.readto_list(file, to_int=True)
+def _sum_three(input: Tuple[int]) -> int:
+    return sum(input)
+
+
+def part1(file: str, input_list: list = None) -> int:
+    _input = Input.readto_list(file, to_int=True) if not input_list else input_list
     _answer = reduce(add, map(_is_higher, zip(_input, _input[1:])))
     return _answer
 
 
 def part2(file: str) -> int:
-    _input = Input.readto_deque(file, to_int=True)
-    _answer = 0
-    _b = deque([_input.popleft() for _ in range(3)])
-    _a = sum(_b)
-    while True:
-        try:
-            _b.popleft()
-            _b.append(_input.popleft())
-            _answer += _is_higher([_a, sum(_b)])
-            _a = sum(_b)
-        except IndexError:
-            return _answer
+    _input = Input.readto_list(file, to_int=True)
+    _answer = part1(None, list(map(_sum_three, zip(_input, _input[1:], _input[2:]))))
+    return _answer
 
 
 if __name__ == "__main__":
